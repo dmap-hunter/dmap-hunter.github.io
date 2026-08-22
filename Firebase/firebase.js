cda1a1efb42cc053372e3a8ced5ea1bed99b6a38
Greendeer30 <markdtgreen@gmail.com>
2026-08-22 17:42:49 -0400

    New Firebase

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -3,13 +3,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCcrrFgfV5am8GYTta60oruuITpjFUertU",
-    authDomain: "dmap-a8cc0.firebaseapp.com",
-    projectId: "dmap-a8cc0",
-    storageBucket: "dmap-a8cc0.firebasestorage.app",
-    messagingSenderId: "78589855261",
-    appId: "1:78589855261:web:aa396e6cc3062c80b2e8f4",
-    measurementId: "G-3FEVPTYPX5"
+  apiKey: "AIzaSyApPjrvUKCNuusAhTMkRpVHi4BaSLXHbRg",
+  authDomain: "dukeforest-96e53.firebaseapp.com",
+  projectId: "dukeforest-96e53",
+  storageBucket: "dukeforest-96e53.firebasestorage.app",
+  messagingSenderId: "807394176812",
+  appId: "1:807394176812:web:e48233f5bc063433efc689",
+  measurementId: "G-Q4V8NRCXBT"
 };
 
 // Initialize Firebase
